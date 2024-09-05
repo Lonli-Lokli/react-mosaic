345beaa72d8609d73f045a532995d7cbd476c839
Lonli-Lokli <lonli.lokli@gmail.com>
2024-09-05 20:50:25 +0100

    I do not like prettier

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'tsup';
-
 export default defineConfig({
-    entryPoints: ['src/index.ts'],
-    format: ['cjs', 'esm'],
-    experimentalDts: true,
-    outDir: 'lib',
-    target: ['es2017', 'node16'],
-    clean: false
-})
+  entryPoints: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  experimentalDts: true,
+  outDir: 'lib',
+  target: ['es2017', 'node16'],
+  clean: false,
+});
