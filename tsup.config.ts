2a8fd2d0aefd60734a8cf0d9d8d8350b77b5b7b5
Lonli-Lokli <lonli.lokli@gmail.com>
2024-09-05 20:33:29 +0100

    adopt new dts generation

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entryPoints: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  experimentalDts: true,
+  outDir: 'lib',
+  target: ['es2017', 'node16'],
+  clean: false,
+});
