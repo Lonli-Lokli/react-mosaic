7810782f1186e6e02baecaa5f17d7dac267ebe5f
Lonli-Loli <lonli.lokli@gmail.com>
2025-06-20 13:37:54 +0100

    chore: :rocket: deploy new version to Github Pages

---
+++ assets/allPathsLoader-B8W-U3Mo.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-Hl8Oxr5x.js","assets/index-BVMreIQm.js","assets/index-Do7YB6C4.js","assets/index-s1QwRASo.js","assets/index-BDrFdDbr.css"])))=>i.map(i=>d[i]);
+import{_ as o,a as n,b as i}from"./index-s1QwRASo.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-Hl8Oxr5x.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
